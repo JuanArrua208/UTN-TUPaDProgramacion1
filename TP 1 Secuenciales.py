1b15a5f1d2c916a1bc4cb71f1abce9bfdea5e28f
JuanArrua208 <108731993+JuanArrua208@users.noreply.github.com>
2025-09-25 19:01:26 -0600

    Subo práctica unidad 1

---
+++ TP 1 Secuenciales.py
@@ -0,0 +1,117 @@
+# Actividad 1:
+# print("Hola Mundo!")
+
+# Actividad 2:
+""" 
+nombre = input("Ingrese su nombre: ")
+print(f"Hola {nombre}!")
+"""
+
+# Actividad 3:
+""" 
+nombre = input("Ingrese su nombre: ")
+appellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+lugarResidencia = input("Ingrese su lugar de residencia: ")
+
+print(f"Hola soy {nombre} {appellido}, tengo {edad} años y vivo en {lugarResidencia}")
+"""
+
+# Actividad 4:
+""" 
+radio = input("Ingrese el radio de un círculo: ")
+radio = float(radio)
+area = 3.14 * (radio * radio)
+perimetro = 2 * 3.14 * radio
+
+print(f"Su Area es {area} y su Perímetro es {perimetro}")
+"""
+
+# Actividad 5:
+""" 
+segundos = input("Ingrese una cantidad de segundos: ")
+segundos = int(segundos)
+horas = segundos / 3600
+
+print(f"{segundos} segundos son {horas} horas")
+"""
+
+#Actividad 6:
+""" 
+numero = input("Ingrese un número: ")
+numero = int(numero)
+uno = numero * 1
+dos = numero * 2
+tres = numero * 3
+cuatro = numero * 4
+cinco = numero * 5
+seis = numero * 6
+siete = numero * 7
+ocho = numero * 8
+nueve = numero * 9
+
+print(f"Tabla del uno: {uno}")
+print(f"Tabla del dos: {dos}")
+print(f"Tabla del tres: {tres}")
+print(f"Tabla del cuatro: {cuatro}")
+print(f"Tabla del cinco: {cinco}")
+print(f"Tabla del seis: {seis}")
+print(f"Tabla del siete: {siete}")
+print(f"Tabla del ocho: {ocho}")
+print(f"Tabla del nueve: {nueve}")
+"""
+
+# Actividad 7:
+""" 
+print("Ingrese dos numeros enteros distintos de cero")
+
+a = input("a: ")
+a = int(a)
+b = input("b: ")
+b = int(b)
+
+suma = a + b
+division = a / b
+multiplicacion = a * b
+resta = a - b
+
+print(f"suma: {suma}")
+print(f"division: {division}")
+print(f"multiplicacion: {multiplicacion}")
+print(f"resta: {resta}")
+"""
+
+# Actividad 8:
+""" 
+altura = input("Ingrese su altura en metros: ")
+altura = float(altura)
+peso = input("Ingrese su peso en kilogramos: ")
+peso = float(peso)
+
+indiceMasaCorporal = peso / (altura * altura)
+
+print(f"Su indice de masa corporal es {indiceMasaCorporal}")
+"""
+
+# Actividad 9:
+""" 
+celcius = input("Ingrese un valor de temperatura en grados celcius: ")
+celcius = float(celcius)
+
+fahrenheit = (9/5) * celcius + 32
+
+print(f"Temperatura engrados Fahrenheit: {fahrenheit}")
+"""
+
+# actividad 10: 
+print("Ingrese 3 números")
+a = input("Ingrese número a: ")
+a = int(a)
+b = input("Ingrese número b: ")
+b = int(b)
+c = input("Ingrese número c: ")
+c = int(c)
+
+promedio = (a + b + c) / 3
+
+print(f"Promedio: {promedio}")
